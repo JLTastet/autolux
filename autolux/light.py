4590a0a01203318a7fd7c1accf45aa85d3d102be
Jean-Loup Tastet <jean-loup.tastet@alumni.epfl.ch>
2019-09-02 21:09:14 +0200

    Do not use the `-ap` options, which have been removed from `light`.

--- autolux/light.py
+++ autolux/light.py
@@ -5,8 +5,8 @@ import opts
 from run_cmd import run_cmd
 
 def set_brightness(new_level, time):
-    run_cmd("light -apS {:f}".format(new_level))
+    run_cmd("light -S {:f}".format(new_level))
 
 def get_brightness():
-    out = run_cmd("light -apG")
+    out = run_cmd("light -G")
     return float(out.strip())
